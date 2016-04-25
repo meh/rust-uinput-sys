e54800bae14106e89f19cff3e4bcccc9fcefd1da
meh <meh@schizofreni.co>
2016-04-25 04:07:56 +0200

    crate: fix ioctl calls

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,35 @@ extern crate libc;
 #[macro_use]
 extern crate ioctl;
 
+use std::mem;
+
+use libc::{c_char, c_int, c_uint, c_ulong};
+use libc::{uint16_t, int32_t, uint32_t};
+use libc::timeval;
+
+macro_rules! uin {
+	(write $name:ident with $ioty:expr, $nr:expr; $ty:ty) => (
+		pub unsafe fn $name(fd: c_int, val: $ty) -> c_int {
+			ioctl::ioctl(fd, iow!($ioty, $nr, mem::size_of::<$ty>()) as c_ulong, val)
+		}
+	);
+}
+
 mod events;
 pub use events::*;
 
-use libc::{c_char, c_int, c_uint, uint16_t, int32_t, uint32_t};
-
 pub const UINPUT_MAX_NAME_SIZE: c_int = 80;
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct input_event {
+	pub time:  timeval,
+	pub kind:  uint16_t,
+	pub code:  uint16_t,
+	pub value: int32_t,
+}
+
+#[derive(Clone, Copy)]
 #[repr(C)]
 pub struct input_id {
 	pub bustype: uint16_t,
@@ -49,17 +70,17 @@ pub struct uinput_user_dev {
 ioctl!(none ui_dev_create with b'U', 1);
 ioctl!(none ui_dev_destroy with b'U', 2);
 
-ioctl!(write ui_set_evbit   with b'U', 100; c_int);
-ioctl!(write ui_set_keybit  with b'U', 101; c_int);
-ioctl!(write ui_set_absbit  with b'U', 102; c_int);
-ioctl!(write ui_set_relbit  with b'U', 103; c_int);
-ioctl!(write ui_set_mscbit  with b'U', 104; c_int);
-ioctl!(write ui_set_ledbit  with b'U', 105; c_int);
-ioctl!(write ui_set_sndbit  with b'U', 106; c_int);
-ioctl!(write ui_set_ffbit   with b'U', 107; c_int);
-ioctl!(write ui_set_phys    with b'U', 108; *const c_char);
-ioctl!(write ui_set_swbit   with b'U', 109; c_int);
-ioctl!(write ui_set_propbit with b'U', 110; c_int);
+uin!(write ui_set_evbit   with b'U', 100; c_int);
+uin!(write ui_set_keybit  with b'U', 101; c_int);
+uin!(write ui_set_absbit  with b'U', 102; c_int);
+uin!(write ui_set_relbit  with b'U', 103; c_int);
+uin!(write ui_set_mscbit  with b'U', 104; c_int);
+uin!(write ui_set_ledbit  with b'U', 105; c_int);
+uin!(write ui_set_sndbit  with b'U', 106; c_int);
+uin!(write ui_set_ffbit   with b'U', 107; c_int);
+uin!(write ui_set_phys    with b'U', 108; *const c_char);
+uin!(write ui_set_swbit   with b'U', 109; c_int);
+uin!(write ui_set_propbit with b'U', 110; c_int);
 
 //ioctl!(readwrite ui_begin_ff_upload with b'U', 200, uinput_ff_upload);
 //ioctl!(readwrite ui_end_ff_upload with b'U', 201, uinput_ff_upload);
